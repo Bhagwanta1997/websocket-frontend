708791a0b7b33128aca66de06ee5fc63940cfbee
Bhagwanta1997 <Bhagwanta.yeole@apptware.com>
2024-03-29 13:23:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import SockJS from 'sockjs-client';
+import { over } from 'stompjs';
+
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  const connectWSS = () => {
+		var sock = new SockJS('https://d11f-2401-4900-5031-c83e-1299-64b4-f25e-6742.ngrok-free.app/ws-endpoint');
+		console.log('sock: ', sock);
+		let stompClient = over(sock);
+		console.log('stompClient: ', stompClient);
+		sock.onopen = function() {
+		console.log('open');
+		}
+		stompClient.connect({}, function (frame) {
+		console.log('Connected: ' + frame);
+		stompClient.subscribe('/topic/notification', function (payload) {
+			console.log(payload.body);
+			//you can execute any function here
+		});
+		});
+	}
+
+	useEffect(() => {
+		connectWSS();
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Welcome to Web Socket Application
     </div>
   );
 }
